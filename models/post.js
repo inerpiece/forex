90998e3318367f9ce3350bab98a65a96b0992fb8
Kizza00232Jera <antonio.jera10@gmail.com>
2020-09-15 16:15:20 +0100

    Class Post

---
+++ models/post.js
@@ -0,0 +1,119 @@
+const con = require('../config/connection');
+const Joi = require('joi');
+const sql = require('mssql');
+// const bcrypt = require('bcryptjs');
+// const crypt = require('../config/encrypt');
+const User = require('./user');
+
+class Post {
+    constructor(postObj) {
+        this.postTitle = postObj.postTitle;
+        this.postBody = postObj.postBody;
+        this.postDate = postObj.postDate;
+        // user: {
+        //     this.userId = userObj.userId;
+        //     this.userEmail = userObj.userEmail;
+        //     this.userFirstName = userObj.userFirstName;
+        //     this.userLastName = userObj.userLastName;
+        //     this.userUsername = userObj.userUsername;
+        //     this.userPhone = userObj.userPhone;
+        //     this.userBirthDay = userObj.userBirthDay;
+        //     if (userObj.role) {
+        //         this.role = {};
+        //         this.role.Id = userObj.role.roleId;
+        //         this.role.roleName = userObj.role.roleName;
+        //         this.role.roleDescription = userObj.role.roleDescription;
+        //     }
+        // }
+    }
+
+    static validate(postObj) {
+        const schema = Joi.object({
+            postTitle: Joi.string().max(255),
+            postBody: Joi.string(),
+            postDate: Joi.string().max(50)
+            // user: Joi.object({
+            //     userId: Joi.number().integer().min(1),
+            //     userEmail: Joi.string().email().max(255),
+            //     userFirstName: Joi.string().max(50),
+            //     userLastName: Joi.string().max(50),
+            //     userUsername: Joi.string().max(50),
+            //     userPhone: Joi.number().integer(),
+            //     userBirthDay: Joi.string().max(50),
+            //     role: Joi.object({
+            //         roleId: Joi.number().integer().min(1),
+            //         roleName: Joi.string().max(50),
+            //         roleDescription: Joi.string().max(255),
+            //     })
+            // })
+        });
+        return schema.validate(postObj);
+    }
+
+    // POST (CREATE) post
+    create(postTextObj) {
+        return new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    const pool = await sql.connect(con);
+                    const result = await pool.request()
+                        .input('postTitle', sql.NVarChar(255), this.postTitle)
+                        .input('postBody', sql.NVarChar(MAX), this.postBody)
+                        .input('postDate', sql.NVarChar(50), this.postDate)
+                        .query(`INSERT INTO forexPost (postTitle, postBody, postDate)
+                            VALUES (@postTitle, @postBody, @postDate)
+                            
+                            SELECT *
+                            FROM forexPost
+                            WHERE forexPost.postID = SCOPE_IDENTITY();`);
+
+                    console.log(result);
+
+                    if (result.recordset.length != 1) throw { statusCode: 500, message: 'DB is corrupt from user.js' };
+
+                    const record = {
+                        postTitle: result.recordset[0].postTitle,
+                        postBody: result.recordset[0].postBody,
+                        postDate: result.recordset[0].postDate
+                        // user: {
+                        //     userId: result.recordset[0].userID,
+                        //     userEmail: result.recordset[0].userEmail,
+                        //     userFirstName: result.recordset[0].userFirstName,
+                        //     userLastName: result.recordset[0].userLastName,
+                        //     userUsername: result.recordset[0].userUsername,
+                        //     userPhone: result.recordset[0].userPhone,
+                        //     userBirthDay: result.recordset[0].userBirthDay,
+                        //     role: {
+                        //         roleId: result.recordset[0].roleID,
+                        //         roleName: result.recordset[0].roleName,
+                        //         roleDescription: result.recordset[0].roleDescription,
+                        //     }
+                        // }
+                    }
+
+                    const { error } = Post.validate(record);
+                    if (error) throw { statusCode: 409, message: error };
+
+                    resolve(new Post(record));
+
+                } catch (err) {
+                    console.log(err);
+                    let errorMessage;
+                    if (!err.statusCode) {
+                        errorMessage = {
+                            statusCode: 500,
+                            message: err + 'we are here'
+                        }
+                    } else {
+                        errorMessage = err;
+                    }
+                    reject(errorMessage);
+                }
+                sql.close();
+            })();
+
+        })
+    }
+}
+
+module.exports = Post;
--- routes/memberPosts.js
+++ routes/memberPosts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Joi = require('joi');
 const sql = require('mssql');
+const Post = require('../models/post');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -17,7 +20,27 @@ router.get('/post/:postId', async (req, res) => {
 // POST requests
 
 router.post('/', async (req, res) => {
+    const postWannabe = req.body;
 
+    try {
+        const validatePost = Post.validate(postWannabe);
+        if(validatePost.error) throw {statusCode: 400, message: validatePost.error};
+
+        const newPost = await new Post(postWannabe);
+        res.send(JSON.stringify(newPost));
+        
+    } catch (err) {
+        let errorMessage;
+            if (!err.statusCode) {
+                errorMessage = {
+                    statusCode: 500,
+                    message: `This is an error: ${err}`
+                };
+            } else {
+                errorMessage = err;
+            }
+            res.status(errorMessage.statusCode).send(JSON.stringify(errorMessage));
+    }
 });
 
 module.exports = router;
